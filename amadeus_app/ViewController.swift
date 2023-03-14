73e3dcc7c5fba2ac34ab703f4fc89fc5326066fa
saudanwar3 <59981578+saudanwar3@users.noreply.github.com>
2023-03-14 23:14:04 +0400

    Initial Project Commit

---
+++ amadeus_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  amadeus_app
+//
+//  Created by Test on 14/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
