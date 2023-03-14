dfa0b4abd13b3fb7ad3f473c13e8de8fefc756a1
saudanwar3 <59981578+saudanwar3@users.noreply.github.com>
2023-03-15 00:19:17 +0400

    Added table view methods

--- amadeus_app/ViewController.swift
+++ amadeus_app/ViewController.swift
@@ -16,12 +16,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.listTableView.dataSource = self
+        self.listTableView.delegate = self
+        self.registerTableViewCells()
+    }
+
+    private func registerTableViewCells() {
+        let nib = UINib(nibName: "CountryTableViewCell",
+                                  bundle: nil)
+        self.listTableView.register(nib,
+                                forCellReuseIdentifier: "CountryTableViewCell")
     }
 
 }
 // MARK: TableView Methods
-extension ViewController
+extension ViewController: UITableViewDelegate, UITableViewDataSource
 {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CountryTableViewCell") as? CountryTableViewCell {
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    
     
 }
